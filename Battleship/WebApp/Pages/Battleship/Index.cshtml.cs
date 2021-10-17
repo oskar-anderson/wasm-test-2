7afb0f1f71e8351ff0267229a22a619c01e8ff60
kaande <kaande@ttu.ee>
2021-10-17 11:08:29 +0300

    js p3

--- Battleship/WebApp/Pages/Battleship/Index.cshtml.cs
+++ Battleship/WebApp/Pages/Battleship/Index.cshtml.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
 
 namespace WebApp.Pages.Battleship
 {
     public class Index : PageModel
     {
-        public void OnGet()
+      private static string StartMenuPartialPath = System.AppContext.BaseDirectory + "Pages\\Shared\\_StartMenuPartial.html"; 
+      private static string NewGamePartialPath = System.AppContext.BaseDirectory + "Pages\\Shared\\_NewGamePartial.html"; 
+      public string StartMenuPartial { get; set; } = System.IO.File.ReadAllText(StartMenuPartialPath); 
+      public string NewGamePartial { get; set; } = System.IO.File.ReadAllText(NewGamePartialPath); 
+        void OnGet()
         {
+         System.Console.WriteLine(System.AppContext.BaseDirectory);
             // System.Diagnostics.Debug.WriteLine("In Page Index, Function OnGet");
         }
     }
